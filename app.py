9d8fe2294c6370e6f89e87006b778606b429745c
sirBarbouchi <51956200+sirBarbouchi@users.noreply.github.com>
2020-04-16 02:29:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from flask import Flask
 from flask import render_template , url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:09869839@localhost/Pfa2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:123@localhost/Pfa2'
 db= SQLAlchemy(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
 
 class Users(db.Model):
-    FirstName = db.Column(db.String(200))
-    LastName = db.Column(db.String(200))
-    Email = db.Column(db.String(200), unique=True)
-    Username = db.Column(db.String(200), primary_key=True)    
-    Password = db.Column(db.String(200))
+    firstName = db.Column(db.String(200))
+    lastName = db.Column(db.String(200))
+    email = db.Column(db.String(200), unique=True)
+    username = db.Column(db.String(200), primary_key=True)
+    password = db.Column(db.String(200))
 
-    def __init__(self, FirstName, LastName, Email, Username, Password):
-        self.FirstName=FirstName
-        self.LastName=LastName
-        self.Email=Email
-        self.Username=Username
-        self.Password=Password
+    def __init__(self, firstName, lastName, email, username, password):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.username = username
+        self.password = password
 
     #def __repr__(self)
 
@@ -29,6 +32,16 @@ class Users(db.Model):
 
 def index():
     return render_template("index.html")
-
+@app.route('/new', methods=['POST', 'GET'])
+def createAccount():
+    firstName = request.form.get('FirstName')
+    lastName = request.form.get('LastName')
+    email = request.form.get('Email')
+    password = request.form.get('Password')
+    username = request.form.get('Username')
+    user = Users(firstName, lastName, email, username, password)
+    db.session.add(user)
+    db.session.commit()
+    return render_template('index.html')
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True, )
