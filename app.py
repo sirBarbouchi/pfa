9930031be1cf9a45791929b18cffe17fd8329314
sirBarbouchi <51956200+sirBarbouchi@users.noreply.github.com>
2020-04-16 03:53:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
-from flask import render_template , url_for
+from flask import (Flask, render_template, request, escape, flash,
+                    url_for, redirect, make_response, session, Blueprint)
 from flask_sqlalchemy import SQLAlchemy
-from flask import request
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:123@localhost/Pfa2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/pfa2'
 db= SQLAlchemy(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = b'\x1d\xcf\n\xe9\x0f*\xe2_Z\xdd\xdb\x19\xd8L\x1e\xaf\x058\x17\xbd\xa1_2\xf4'
 
 
 class Users(db.Model):
@@ -31,17 +32,18 @@ class Users(db.Model):
 @app.route('/')
 
 def index():
-    return render_template("index.html")
+    return render_template("registration.html")
 @app.route('/new', methods=['POST', 'GET'])
 def createAccount():
-    firstName = request.form.get('FirstName')
-    lastName = request.form.get('LastName')
-    email = request.form.get('Email')
-    password = request.form.get('Password')
-    username = request.form.get('Username')
-    user = Users(firstName, lastName, email, username, password)
-    db.session.add(user)
-    db.session.commit()
-    return render_template('index.html')
+    if request.method == "POST":
+        firstName = request.form.get('FirstName')
+        lastName = request.form.get('LastName')
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+        username = request.form.get('Username')
+        user = Users(firstName, lastName, email, username, password)
+        db.session.add(user)
+        db.session.commit()
+    return render_template("index.html")
 if __name__== "__main__":
-    app.run(debug=True, )
+    app.run(debug=True)
