fb0cd29756bc8111dc82129537cfc2b730744a1d
sirBarbouchi <51956200+sirBarbouchi@users.noreply.github.com>
2020-05-01 00:44:46 +0100

    Update login.py

--- views/login.py
+++ views/login.py
@@ -32,7 +32,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('auth.login'))
     return render_template('registration.html', title='Register', form=form)
 
 
